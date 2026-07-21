d969cfde8a038f86c80245149773582c3ace3158
Hacktiv8 <hacktiv8@Hacktiv8s-MacBook-Air.local>
2026-07-21 13:15:40 +0700

    feat: add endpoint register

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/register", (req, res) => {
+  res.send("Hello Register!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
